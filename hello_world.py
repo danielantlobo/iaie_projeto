6714da23d64e0c6a25b2157c1cb1cbf4eec95105
Daniel Lobo <danielantoniopintolobo@gmail.com>
2024-12-10 18:44:35 +0000

    second commit.

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+from flask import Flask, jsonify, request
+import requests
+import random
+
+app = Flask(__name__)
+
+# Ligar o servidor
+if __name__ == '__main__':
+    register_producer() # Registar produtor
+    app.run(host="127.0.0.1", port=6666)
